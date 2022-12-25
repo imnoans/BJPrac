bb570bdf4d8c01550917799c99844db790e3d392
noanswer <sy1004da@gmail.com>
2022-12-26 01:18:38 +0900

    No_11022

---
+++ No_11022/src/First.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class First
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t, a, b, c;
+        do {
+            t = sc.nextInt();
+        }while(t < 0);
+
+        for(int i = 1; i <= t; i++)
+        {
+            do {
+                a = sc.nextInt();
+            }while(a <= 0);
+
+            do{
+                b = sc.nextInt();
+            }while(b >= 10);
+
+            c = a + b;
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + c);
+        }
+    }
+}
