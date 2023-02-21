db88df7a660aa275916575422321b64d4df567c7
noanswer <sy1004da@gmail.com>
2023-02-21 20:30:52 +0900

    No_15657

---
+++ 백트래킹/Silver_3/No_15657_NM8/src/Main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static int[] nums;
+    public static int n, m;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        nums = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+        {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        dfs(0,0);
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    public static void dfs(int v, int count)
+    {
+        if(count == m)
+        {
+            for(int i = 0; i < m; i++)
+            {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = v; i < n; i++)
+        {
+            arr[count] = nums[i];
+            dfs(i,count + 1);
+        }
+    }
+}
